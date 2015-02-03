efc43562470406eaa687923e5d476dc25c1ee1f9
Amra <amrapop@gmail.com>
2015-02-03 14:30:59 +0100

    Add HandShake

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -3,8 +3,10 @@ package Client;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 import GUI.ChatGui;
+
 
 public class Client {
 	public static final int port = 1717;
@@ -16,6 +18,10 @@ public class Client {
 		try {
 			
 			Socket client = new Socket(host, port);
+			System.out.println("Unesi svoje ime: ");
+			Scanner scan =  new Scanner(System.in);
+			String name = scan.nextLine()+"\n";   // zato sto koristimo BufferedReader koji ceka da dodje do new Line --> "\n"
+			client.getOutputStream().write(name.getBytes());
 			ChatGui gui = new ChatGui(client);
 			new Thread(gui).start();
 			
